3656febd2ae3efc38df7063c38abbc1008a9fb39
Levine Wang <wang_levine@outlook.com>
2023-07-04 22:01:14 -0700

    Add first name

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,7 +1,30 @@
+import { useState } from "react";
 import Header from "./Header";
 
 const Form = () => {
-  return <Header />;
+  const [firstName, setFirstName] = useState("");
+
+  return (
+    <>
+      <Header />
+      <div className="row">
+        <form className="col s12">
+          <div className="row">
+            <div className="input-field col s6">
+              <input
+                id="firstName"
+                type="text"
+                className="validate"
+                value={firstName}
+                onChange={(e) => setFirstName(e.target.value)}
+              />
+              <label htmlFor="firstName">First Name</label>
+            </div>
+          </div>
+        </form>
+      </div>
+    </>
+  );
 };
 
 export default Form;
