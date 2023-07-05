a3a8139fc1c75efd4bda5f929d98be716ccb2a8f
Levine Wang <wang_levine@outlook.com>
2023-07-05 13:50:25 -0700

    Add ReCAPTCHA

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
+import ReCAPTCHA from "react-google-recaptcha";
 
 const Form = () => {
   const [firstName, setFirstName] = useState("");
@@ -11,6 +12,7 @@ const Form = () => {
   const [country, setCountry] = useState("");
   const [post, setPost] = useState("");
   const [area, setArea] = useState("");
+  const captchaRef = useRef(null);
 
   return (
     <form method="POST">
@@ -163,35 +165,10 @@ const Form = () => {
         </div>
       </div>
 
-      <div className="formbold-checkbox-wrapper">
-        <label htmlFor="supportCheckbox" className="formbold-checkbox-label">
-          <div className="formbold-relative">
-            <input
-              type="checkbox"
-              id="supportCheckbox"
-              className="formbold-input-checkbox"
-            />
-            <div className="formbold-checkbox-inner">
-              <span className="formbold-opacity-0">
-                <svg
-                  width="11"
-                  height="8"
-                  viewBox="0 0 11 8"
-                  fill="none"
-                  className="formbold-stroke-current"
-                >
-                  <path
-                    d="M10.0915 0.951972L10.0867 0.946075L10.0813 0.940568C9.90076 0.753564 9.61034 0.753146 9.42927 0.939309L4.16201 6.22962L1.58507 3.63469C1.40401 3.44841 1.11351 3.44879 0.932892 3.63584C0.755703 3.81933 0.755703 4.10875 0.932892 4.29224L0.932878 4.29225L0.934851 4.29424L3.58046 6.95832C3.73676 7.11955 3.94983 7.2 4.1473 7.2C4.36196 7.2 4.55963 7.11773 4.71406 6.9584L10.0468 1.60234C10.2436 1.4199 10.2421 1.1339 10.0915 0.951972ZM4.2327 6.30081L4.2317 6.2998C4.23206 6.30015 4.23237 6.30049 4.23269 6.30082L4.2327 6.30081Z"
-                    strokeWidth="0.4"
-                  ></path>
-                </svg>
-              </span>
-            </div>
-          </div>
-          I agree to the defined
-          <a href="#"> terms, conditions, and policies</a>
-        </label>
-      </div>
+      <ReCAPTCHA
+        sitekey="6Le_Qu0mAAAAAFjsfGnbaNrYXugjXlQLzMxNHiS5"
+        ref={captchaRef}
+      />
 
       <button className="formbold-btn">Register Now</button>
     </form>
